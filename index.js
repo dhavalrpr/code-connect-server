171aa66e21fd6bc7340398fe0904997e39a41a7e
dhavalrpr <dhaval.inbweb25@gmail.com>
2025-02-25 15:36:52 +0530

    Updated port

--- index.js
+++ index.js
@@ -409,7 +409,7 @@ process.on('unhandledRejection', (reason, promise) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8000;
+const PORT = 8080;
 server.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
 
 module.exports = { ACTIONS };
